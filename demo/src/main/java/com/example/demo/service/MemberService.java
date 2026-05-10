7cc1d24ee971e99df314e4bb54f1dd0ff11399e3
hmc1206 <67060344+hmc1206@users.noreply.github.com>
2026-05-10 10:51:01 +0900

    Docs
    Add comments explaining MemberService.java

--- demo/src/main/java/com/example/demo/service/MemberService.java
+++ demo/src/main/java/com/example/demo/service/MemberService.java
@@ -25,41 +25,66 @@ public class MemberService {
         //memberRepository.findByUserId() : 아이디로 DB에 가서 회원 객체를 가져옴(member)
     }
 
+    //모든 회원 정보를 리스트형식으로 가져옴
     public List<Member> findAll() {return memberRepository.findAll();}
 
+    //memberId를 받아서 그값에 맞는 회원의 id를 찾음
     public Member findById(Long memberId) {return memberRepository.findById(memberId);}
 
+    //id,newName,newPassword를 매개변수로 받음
     public void update(Long id, String newName, String newPassword){
+
+        //DB에서 id에 맞는 회원을 찾아서 member객체로 읽어오기
         Member member = memberRepository.findById(id);
 
+        //member객채에서 username을 newName으로 변경
         member.setUsername(newName);
+
+        //newPassword가 null,empty인지 확인
         if(newPassword != null && !newPassword.isEmpty()){
+            //보안 토큰 작성
             String hashePassword = BCrypt.hashpw(newPassword, BCrypt.gensalt());
+            //member객체에서 password에 새로운 password로 변경
             member.setPassword(hashePassword);
         }
 
+        //DB에 새 값으로 생성된 member의 정보를 저장
         memberRepository.save(member);
     }
 
     public Long signUp(Member member){
+        //BCrypt.gensalt : 암호활때마다 매번 다른 무작위값 생성
+        //BCrypt.hashpw : member.getPassowr()값과 BCrypt.gensalt()의 값을 석어서 암호문 생성
         String hashedPassword = BCrypt.hashpw(member.getPassword(), BCrypt.gensalt());
+        //memder의 password에 암호문 저장
         member.setPassword(hashedPassword);
+        //DB에 member의 값을 저장
         memberRepository.save(member);
+
+        //member의 id를 반환함
         return member.getId();
     }
 
+
+    //memberRepository(=>DB)에서 매개변수로 받은 id에 맞는 회원정보를 삭제
     public void delete(Long id) { memberRepository.remove(id);}
 
     public String login(String userId, String password){
+        //memberRepository에서 userid에 맞는 회원을 찾아서 member객체로 읽음
         Member member = memberRepository.findByUserId(userId);
 
+        //member의 값이 null이 아니거나 BCrypt.checkpw(password, member.getPassword())의 값이 0이 아닌지 확인
         if(member != null && BCrypt.checkpw(password, member.getPassword())){
+            //JWT 토큰 발급
             String token = jwtUtil.generateJwt(member.getUserId(), member.getUsername());
+            //토큰 반환
             return token;
         }
 
+        //아이디나 비밀번호가 맞지 않으면 반환
         return "아이디와 비밀번호를 확인하세요";
     }
 
+    //?
     public  Member findByUserId(String userId) {return memberRepository.findByUserId(userId);}
 }
