aa2230a8eb251f9bb718fcab9a47b5101c814918
hmc1206 <67060344+hmc1206@users.noreply.github.com>
2026-05-11 15:01:20 +0900

    Docs
    Add comments explaining Member.java

--- demo/src/main/java/com/example/demo/domain/Member.java
+++ demo/src/main/java/com/example/demo/domain/Member.java
@@ -5,15 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+//실제 값
 @Setter
 @Getter
 @NoArgsConstructor
 public class Member {
+    //DB에 자동으로 생성되는 순번(?)
     private Long id;
+    //userId => ex)hong01
     private String userId;
+    //password => ex)1234
     private String password;
+    //username => ex)"홍길동";
     private String username;
 
+    //member 클래스의 사용자 정의 생성자(초기화)
     public Member(String userId, String password, String username){
         this.userId = userId;
         this.password = password;
